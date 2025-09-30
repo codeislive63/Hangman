780d414807a10065a97bcc2f2a9e9d720bee7a03
codeislive63 <codeislive63@gmail.com>
2025-10-01 01:22:41 +0300

    update project files and Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,39 @@
-﻿//todo: Разработать код игры "Виселица" для корректного ответа
-Console.WriteLine("Hello, World!");
+﻿using Hangman.Application.Abstractions;
+using Hangman.Application.Services;
+using Hangman.Controllers;
+using Hangman.Extensions.DependencyInjection;
+using Hangman.Extensions.Hosting;
+using Hangman.Extensions.Infrastructure;
+using Hangman.Extensions.Logging;
+using Hangman.Infrastructure.Repositories;
+
+var builder = HostFactory.CreateBuilder(args);
+builder.UseLogger(new FileLogger(Path.Combine(AppContext.BaseDirectory, "logs.txt")));
+
+// ===== НЕИНТЕРАКТИВНЫЙ РЕЖИМ =====
+if (args.Length == 2)
+{
+    builder.ConfigureServices(services =>
+    {
+        services.AddSingleton<IController, NonInteractiveController>();
+    });
+
+    var app = builder.Build();
+    app.Run();
+    return;
+}
+
+// ===== ИНТЕРАКТИВНЫЙ РЕЖИМ =====
+builder.LoadConfigurationFromEncrypted();
+
+builder.ConfigureServices(services =>
+{
+    services.AddSingleton<IWordRepository, WordRepository>();
+    services.AddSingleton<IGameSetupService, GameSetupService>();
+});
+
+// views + оба контроллера; Host выберет нужный контроллер по CanHandle
+builder.AddControllersWithViews();
+
+var interactiveApp = builder.Build();
+interactiveApp.Run();
